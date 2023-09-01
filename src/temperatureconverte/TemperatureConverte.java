80fbadced6ed864248426b85fb0d6a359439339f
Samsa <samsa.programmer@gmail.com>
2023-09-02 02:23:05 +0530

    Temp

---
+++ src/temperatureconverte/TemperatureConverte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package temperatureconverte;
+
+/**
+ *
+ * @author Samsa
+ */
+public class TemperatureConverte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Temconv tc =new Temconv();
+        tc.setVisible(true);
+    }
+    
+}
